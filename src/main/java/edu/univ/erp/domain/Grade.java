a13c6f19d382679f407d0bd8f1eccd8f32cefad7
Anmol-Saluja <anmol24085@iiitd.ac.in>
2025-11-02 23:50:31 +0530

    Update Grade.java

--- src/main/java/edu/univ/erp/domain/Grade.java
+++ src/main/java/edu/univ/erp/domain/Grade.java
@@ -1,4 +1,29 @@
 package edu.univ.erp.domain;
 
-public class Grade {
+public class GradeComponent {
+    private int componentId;
+    private int courseId;
+    private String name;
+    private double weight;
+
+    public GradeComponent() {}
+
+    public GradeComponent(int componentId, int courseId, String name, double weight) {
+        this.componentId = componentId;
+        this.courseId = courseId;
+        this.name = name;
+        this.weight = weight;
+    }
+
+    public int getComponentId() { return componentId; }
+    public void setComponentId(int componentId) { this.componentId = componentId; }
+
+    public int getCourseId() { return courseId; }
+    public void setCourseId(int courseId) { this.courseId = courseId; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public double getWeight() { return weight; }
+    public void setWeight(double weight) { this.weight = weight; }
 }
