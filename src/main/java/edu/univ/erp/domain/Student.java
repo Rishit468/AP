432b003dd4f609db05080eaf19701c869367dcd4
Anmol-Saluja <anmol24085@iiitd.ac.in>
2025-11-02 23:49:16 +0530

    Update Student.java

--- src/main/java/edu/univ/erp/domain/Student.java
+++ src/main/java/edu/univ/erp/domain/Student.java
@@ -1,4 +1,29 @@
 package edu.univ.erp.domain;
 
 public class Student {
+    private int studentId;
+    private String name;
+    private String email;
+    private String department;
+
+    public Student() {}
+
+    public Student(int studentId, String name, String email, String department) {
+        this.studentId = studentId;
+        this.name = name;
+        this.email = email;
+        this.department = department;
+    }
+
+    public int getStudentId() { return studentId; }
+    public void setStudentId(int studentId) { this.studentId = studentId; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getDepartment() { return department; }
+    public void setDepartment(String department) { this.department = department; }
 }
