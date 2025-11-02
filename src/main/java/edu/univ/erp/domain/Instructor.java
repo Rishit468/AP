e1fe495d4d7cade0fce8910ef1fa910b344624ea
Anmol-Saluja <anmol24085@iiitd.ac.in>
2025-11-02 23:49:38 +0530

    Update Instructor.java

--- src/main/java/edu/univ/erp/domain/Instructor.java
+++ src/main/java/edu/univ/erp/domain/Instructor.java
@@ -1,4 +1,29 @@
 package edu.univ.erp.domain;
 
 public class Instructor {
+    private int instructorId;
+    private String name;
+    private String email;
+    private String department;
+
+    public Instructor() {}
+
+    public Instructor(int instructorId, String name, String email, String department) {
+        this.instructorId = instructorId;
+        this.name = name;
+        this.email = email;
+        this.department = department;
+    }
+
+    public int getInstructorId() { return instructorId; }
+    public void setInstructorId(int instructorId) { this.instructorId = instructorId; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getDepartment() { return department; }
+    public void setDepartment(String department) { this.department = department; }
 }
