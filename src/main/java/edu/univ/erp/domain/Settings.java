33474bc46912610b84df72fcb6a3fccb9ec10541
Rishit468 <rishit24468@iiitd.ac.in>
2025-11-10 16:51:26 +0530

    settings

--- src/main/java/edu/univ/erp/domain/Settings.java
+++ src/main/java/edu/univ/erp/domain/Settings.java
@@ -1,4 +1,29 @@
 package edu.univ.erp.domain;
 
 public class Settings {
+    // This maps to the 'settings' table in the ERP DB
+    private boolean maintenanceMode;
+
+    public Settings() {
+        this.maintenanceMode = false;
+    }
+
+    public Settings(boolean maintenanceMode) {
+        this.maintenanceMode = maintenanceMode;
+    }
+
+    public boolean isMaintenanceMode() {
+        return maintenanceMode;
+    }
+
+    public void setMaintenanceMode(boolean maintenanceMode) {
+        this.maintenanceMode = maintenanceMode;
+    }
+
+    @Override
+    public String toString() {
+        return "Settings{" +
+                "maintenanceMode=" + maintenanceMode +
+                '}';
+    }
 }
